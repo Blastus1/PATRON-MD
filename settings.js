bec8b0374464686cf7446e0b4ec198c3dc7bbcee
Blastus1 <kenzysommy@gmail.com>
2025-01-06 13:10:42 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 global.bankname = "MONIEPOINT"
 
-global.banknumber = "8133729715"
+global.banknumber = "8100366428"
 
-global.bankowner = "FADARE"
+global.bankowner = "SOMTOCHUKWU OBINNA UMEASIEGBU"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
